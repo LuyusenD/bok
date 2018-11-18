86ad6b25fc3282b38ad606adebe5f50e2e0f3575
TinJ <863187334@qq.com>
2018-11-19 00:38:26 +0800

    0:38

--- routes/comment.js
+++ routes/comment.js
@@ -7,10 +7,31 @@ router.post('/add',(req,res)=>{
     var sql = `INSERT INTO bok_comment VALUES (NULL, ?, ?, ?, ?);`
     var data = new Date().toLocaleString();
     var {id,uid,content} = req.body;
-    pool.query(sql,[id,uid,data,content],(err,result)=>{
-        result.length>0?res.send({'code':'200','msg':'add success'}):res.send({'code':'404','msg':'add error'})
-    })
+		if(content =='' || content == null || uid ==null || id==null){
+					res.send({'code':'404','msg':'add error'})
+            		return;
+	}
+	var p = new Promise(function(open){
+		var sql = `SELECT comment FROM bok_article WHERE id = ?`;
+		pool.query(sql,[id],(err,result)=>{
+			open(result[0].comment)
+		})
+	})
+	.then(function(msg){
+		msg = parseInt(msg)+1
+		var sql = `UPDATE bok_article SET comment = ? WHERE id = ?`;
+		pool.query(sql,[msg,id],(err,result)=>{
+
+		})
+	})
+	.then(function(){
+		pool.query(sql,[id,uid,data,content],(err,result)=>{
+        result.affectedRows>0?res.send({'code':'200','msg':'add success'}):res.send({'code':'404','msg':'add error'})
+        })	
+	})
+
 })
+
 
 
 
